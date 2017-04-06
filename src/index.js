5f86b8d7888d4b106daddd751b64c5a790459182
Spain <spain@myhomezen.com>
2017-04-05 20:29:35 -0400

    safe indexof

--- src/index.js
+++ src/index.js
@@ -24,6 +24,39 @@ const objectKeys = require('./object-keys');
 const isArray = (arg) =>
     Object.prototype.toString.call(arg) === '[object Array]';
 
+// Production steps of ECMA-262, Edition 5, 15.4.4.14
+// Reference: http://es5.github.io/#x15.4.4.14
+const indexOf = (arr, searchElement, fromIndex) => {
+  var k;
+
+  if (arr == null) {
+    throw new TypeError('"arr" is null or not defined');
+  }
+
+  var o = Object(arr);
+  var len = o.length >>> 0;
+
+  if (len === 0) {
+    return -1;
+  }
+
+  var n = fromIndex | 0;
+
+  if (n >= len) {
+    return -1;
+  }
+
+  k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+  while (k < len) {
+    if (k in o && o[k] === searchElement) {
+      return k;
+    }
+    k++;
+  }
+  return -1;
+};
+
 // This constructor is used to store parsed query string values. Instantiating
 // this is faster than explicitly calling `Object.create(null)` to get a
 // "clean" empty object (tested with v8 v4.9).
@@ -332,7 +365,7 @@ function parse(qs, sep, eq, options) {
         if (key || value || lastPos - posIdx > sepLen || i === 0) {
           // Use a key array lookup instead of using hasOwnProperty(), which is
           // slower
-          if (keys.indexOf(key) === -1) {
+          if (indexOf(keys, key) === -1) {
             obj[key] = value;
             keys[keys.length] = key;
           } else {
@@ -441,7 +474,7 @@ function parse(qs, sep, eq, options) {
       value = decodeStr(value, decode);
     // Use a key array lookup instead of using hasOwnProperty(), which is
     // slower
-    if (keys.indexOf(key) === -1) {
+    if (indexOf(keys, key) === -1) {
       obj[key] = value;
       keys[keys.length] = key;
     } else {
@@ -469,67 +502,4 @@ function decodeStr(s, decoder) {
   } catch (e) {
     return QueryString.unescape(s, true);
   }
-}
-
-
-// Production steps of ECMA-262, Edition 5, 15.4.4.14
-// Reference: http://es5.github.io/#x15.4.4.14
-if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function(searchElement, fromIndex) {
-
-    var k;
-
-    // 1. Let o be the result of calling ToObject passing
-    //    the this value as the argument.
-    if (this == null) {
-      throw new TypeError('"this" is null or not defined');
-    }
-
-    var o = Object(this);
-
-    // 2. Let lenValue be the result of calling the Get
-    //    internal method of o with the argument "length".
-    // 3. Let len be ToUint32(lenValue).
-    var len = o.length >>> 0;
-
-    // 4. If len is 0, return -1.
-    if (len === 0) {
-      return -1;
-    }
-
-    // 5. If argument fromIndex was passed let n be
-    //    ToInteger(fromIndex); else let n be 0.
-    var n = fromIndex | 0;
-
-    // 6. If n >= len, return -1.
-    if (n >= len) {
-      return -1;
-    }
-
-    // 7. If n >= 0, then Let k be n.
-    // 8. Else, n<0, Let k be len - abs(n).
-    //    If k is less than 0, then let k be 0.
-    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-    // 9. Repeat, while k < len
-    while (k < len) {
-      // a. Let Pk be ToString(k).
-      //   This is implicit for LHS operands of the in operator
-      // b. Let kPresent be the result of calling the
-      //    HasProperty internal method of o with argument Pk.
-      //   This step can be combined with c
-      // c. If kPresent is true, then
-      //    i.  Let elementK be the result of calling the Get
-      //        internal method of o with the argument ToString(k).
-      //   ii.  Let same be the result of applying the
-      //        Strict Equality Comparison Algorithm to
-      //        searchElement and elementK.
-      //  iii.  If same is true, return k.
-      if (k in o && o[k] === searchElement) {
-        return k;
-      }
-      k++;
-    }
-    return -1;
-  };
 }
